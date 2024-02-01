65cd64a31647ea5e2ab2b637fb8c79bdbcfbb93a
forgetme-not <gijae8888@naver.com>
2024-02-01 11:26:37 +0900

    pgj 메인페이지 완성

--- mainpage/default.js
+++ mainpage/default.js
@@ -1,37 +1,27 @@
-const container = document.querySelector("#container"); // 캐러셀 영역
-const pics = ["c-1.jpg", "c-2.jpg"];  // 이미지 배열
-
-// 첫번째 이미지 추가
-const img = document.createElement("img");
-img.src = `image/${pics[0]}`;
-container.appendChild(img);
-
-const arrows = document.querySelectorAll(".arrow");  // 화살표
-let i = 0;  // 배열 인덱스
+const container = document.querySelector(".container");
+const pics = ["cinema1.png", "cinema2.png", "cinema3.png"];
+const img = document.querySelector(".carousel-img");
+let i = 0;
 
 function changeImage(direction) {
-  if (direction === "left") {
-    i--; // 이전 이미지로 이동
+  if (direction === "prev") {
+    i--;
     if (i < 0) {
-      i = pics.length - 1;  // 맨 마지막 이미지로 이동
-    }      
-  } else if (direction === "right") {
-    i++;  // 다음 이미지로 이동
+      i = pics.length - 1;
+    }
+  } else if (direction === "next") {
+    i++;
     if (i >= pics.length) {
-      i = 0;  // 첫번째 이미지로 이동
+      i = 0;
     }
   }
-  img.src = `image/${pics[i]}`;  // 현재 이미지 표시
+  img.src = `image/${pics[i]}`;
 }
 
-arrows.forEach(arrow => {
-  arrow.addEventListener("click", (e) => {
-    const direction = e.target.id;
-    changeImage(direction);
-  });
-});
+function autoChangeImage() {
+  setInterval(() => {
+    changeImage("next");
+  }, 2000); // 3초마다 이미지 변경
+}
 
-// 1초마다 자동으로 이미지 변경
-setInterval(() => {
-  changeImage("right");
-}, 3000);
+autoChangeImage();
